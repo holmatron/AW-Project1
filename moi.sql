d52c8d95b0ddff05b6202463491a0b63aa4fad53
Emma King <49696513+EmmaKingDev@users.noreply.github.com>
2022-07-05 14:08:16 +0300

    Delete moi.sql

--- moi.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE timestamps (
-    id SERIAL PRIMARY KEY,
-    start_date TIMESTAMP NOT NULL,
-    start_time TIME NOT NULL,
-    end_date TIMESTAMP NOT NULL,
-    end_time TIME NOT NULL,
-    project VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL
-);
